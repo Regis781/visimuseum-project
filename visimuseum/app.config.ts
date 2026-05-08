821ce075e3ce3fa80528cbf5bc1be325316eb6f5
Regis781 <regismo78reau@gmail.com>
2026-05-08 21:02:07 +0200

    Refactor Vite configuration to use a function

--- visimuseum/app.config.ts
+++ visimuseum/app.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "@tanstack/react-start/config";
 import tsConfigPaths from "vite-tsconfig-paths";
-
+ 
 export default defineConfig({
   tsr: {
     appDirectory: "src",
   },
-  vite: {
+  vite: () => ({
     plugins: [tsConfigPaths()],
-  },
+  }),
 });
+ 
